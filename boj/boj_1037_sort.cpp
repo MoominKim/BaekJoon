fb2e54a418347f6c85677ed8cb282e036d4ba841
MoominKim <seagraml79@gmail.com>
2021-05-29 15:49:04 +0900

    1037

---
+++ boj/boj_1037_sort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    vector<int> v(N);
+    for(int i=0;i<N;i++) cin>>v[i];
+    sort(v.begin(),v.end());
+    cout<<(*v.begin())*(*(v.end()-1));
+}
